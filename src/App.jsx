ea074848ea11953d3a443111571a2120f3d38eeb
anshulbarapatre84 <anshulbarapatre84@gmail.com>
2025-05-02 20:48:28 +0530

    Add react-router-dom dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home pages/Home';
-import Services from './pages/Services pages/Services';
-import Contact from './pages/ContactUs pages/Contact';
+import Home from './pages/Homepages/Home';
+import Services from './pages/Servicespages/Services';
+import Contact from './pages/ContactUspages/Contact';
 // import About from './pages/About Pages/AboutUs'/;
 
 import Navbar from './component/Navbar';
 import Footer from './component/Footer';
-import About from './pages/About Pages/aboutUs';
+import About from './pages/AboutPages/Aboutus';
 
 const  App =() => {
   return (
