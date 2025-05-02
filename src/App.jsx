8516d088e35b8842bbf99b8f9a656c4e2cd08f52
anshulbarapatre84 <anshulbarapatre84@gmail.com>
2025-05-02 15:31:24 +0530

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home pages/Home';
 import Services from './pages/Services pages/Services';
 import Contact from './pages/ContactUs pages/Contact';
-import About from './pages/About Pages/AboutUs';
+// import About from './pages/About Pages/AboutUs'/;
 
 import Navbar from './component/Navbar';
 import Footer from './component/Footer';
+import About from './pages/About Pages/aboutUs';
 
 const  App =() => {
   return (
